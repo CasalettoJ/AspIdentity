b1cdeb579491eae7e92ad7020b2540537103ef94
Jcasaletto <jCasaletto@springthrough.com>
2015-05-19 15:33:49 -0400

    installation of required references, creation of service project

---
+++ AngularandCSS.Service/ServiceBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngularandCSS.Data;
+
+namespace AngularandCSS.Service
+{
+    public class ServiceBase
+    {
+        protected DataContext _dataContext;
+
+        public ServiceBase(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
---
+++ AngularandCSS.Service/UserService.cs
@@ -0,0 +1,23 @@
+﻿using AngularandCSS.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularandCSS.Service
+{
+    public class UserService : ServiceBase
+    {
+        private UserManager<User> _userManager { get; set; }
+        private RoleManager<IdentityRole> _roleManager { get; set; }
+
+        public UserService( DataContext dataContext) : base(dataContext)
+        {
+            _userManager = new UserManager<User>(new UserStore<User>(dataContext));
+            _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(dataContext));
+        }
+    }
+}
